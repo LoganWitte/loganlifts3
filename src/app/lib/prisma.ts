94f67ffddc5215aea6ca4678b7f431d9fabc438d
LoganWitte <156727477+LoganWitte@users.noreply.github.com>
2026-08-25 03:36:28 -0400

    Initialization complete
    Minor configurations

---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@prisma/client' // Error until schema creation & prisma generate
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
